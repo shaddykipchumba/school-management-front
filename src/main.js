2e0cd2e1fd592face57812ad63dca3a976d6dd58
shaddykipchumba <skipchumba053@gmail.com>
2023-07-06 15:39:52 -0700

    reconfiguring to start developing front end of school management system

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import './assets/main.css'
+
 
 import { createApp } from 'vue'
+import './axios'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+
+const pinia = createPinia();
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.use(pinia);
+app.use(router);
+
 
 app.mount('#app')
